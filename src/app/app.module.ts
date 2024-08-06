0526c0e49d3f37c9c5b9993fd064a7dc855ff3a0
Alexandra <alexa.dayanna07@gmail.com>
2024-08-06 11:35:36 -0500

    feat: all components added in screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { NavbarComponent } from './layouts/components/navbar/navbar.component';
-import { FooterComponent } from './layouts/components/footer/footer.component';
-import { ScreenComponent } from './layouts/screen/screen.component';
+import { NavbarComponent } from './layouts/core/components/navbar/navbar.component';
+import { FooterComponent } from './layouts/core/components/footer/footer.component';
+import { ScreenComponent } from './layouts/screen/pages/screen.component';
+import { HeroSectionComponent } from './layouts/screen/components/hero-section/hero-section.component';
+import { AboutMeComponent } from './layouts/screen/components/about-me/about-me.component';
+import { SkillsComponent } from './layouts/screen/components/skills/skills.component';
+import { ProjectsComponent } from './layouts/screen/components/projects/projects.component';
+import { DesignsComponent } from './layouts/screen/components/designs/designs.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FooterComponent,
-    ScreenComponent
+    ScreenComponent,
+    HeroSectionComponent,
+    AboutMeComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    DesignsComponent
   ],
   imports: [
     BrowserModule,
--- src/app/layouts/screen/pages/screen.component.ts
+++ src/app/layouts/screen/pages/screen.component.ts
@@ -6,7 +6,5 @@ import { Component } from '@angular/core';
   styleUrl: './screen.component.scss',
 })
 export class ScreenComponent {
-
   constructor() { }
-
 }
