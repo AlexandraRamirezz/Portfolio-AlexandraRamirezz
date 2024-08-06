d0ea4f33477e37bb7b5b80d7067261860797e9b7
Alexandra <alexa.dayanna07@gmail.com>
2024-08-06 11:33:26 -0500

    feat: the portfolio designs were created with links and images

---
+++ src/app/layouts/screen/components/designs/designs.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-designs',
+  templateUrl: './designs.component.html',
+  styleUrl: './designs.component.scss',
+})
+export class DesignsComponent {
+  constructor() { }
+}
